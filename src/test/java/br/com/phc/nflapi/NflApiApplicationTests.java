30fb0dd4659b47fd3b6901f113602ea2110eb943
fernandachimenez21 <fernanda.leme@bandtec.com.br>
2021-11-03 13:38:29 -0300

    Upload versao inicial do projeto

---
+++ src/test/java/br/com/phc/nflapi/NflApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.phc.nflapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NflApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
