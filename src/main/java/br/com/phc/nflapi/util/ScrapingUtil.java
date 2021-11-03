10a4aaaceed1485c3995cea5b0cec7f2ca4efb48
fernandachimenez21 <fernanda.leme@bandtec.com.br>
2021-11-03 15:45:45 -0300

    Ajustes da API - LOGGER

--- src/main/java/br/com/phc/nflapi/util/ScrapingUtil.java
+++ src/main/java/br/com/phc/nflapi/util/ScrapingUtil.java
@@ -1,20 +1,47 @@
 package br.com.phc.nflapi.util;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import br.com.phc.nflapi.dto.PartidaGoogleDto;
 
 public class ScrapingUtil {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(ScrapingUtil.class);
+
 	private static final String BASE_URL_GOOGLE = "https://www.google.com.br/search?q=";
 	private static final String COMPLEMENTO_URL_GOOGLE = "&hl=pt-BR";
-	
-			
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		PartidaGoogleDto partida = new PartidaGoogleDto();
-		
-		partida.getGolsEquipeCasa();
 
+	public static void main(String[] args) {
+		String url = BASE_URL_GOOGLE + "chiefs+e+bills+10/10/2021" + COMPLEMENTO_URL_GOOGLE;
+
+		ScrapingUtil scraping = new ScrapingUtil();
+		scraping.obtemInformacoesPartida(url);
+	}
+
+	public PartidaGoogleDto obtemInformacoesPartida(String url) {
+		PartidaGoogleDto partida = new PartidaGoogleDto();
+
+		Document document = null;
+
+		// Se houve exceção, printará essa mensagem de erro ao usuário
+		try {
+			document = Jsoup.connect(url).get();
+
+			// Recuperando título da página
+			String title = document.title();
+			LOGGER.info("Título da página: {}", title);
+			
+		} catch (IOException e) {
+			LOGGER.error("ERRO AO TENTAR CONECTAR NO GOOGLE COM JSOUP -> {}", e.getMessage());
+			e.printStackTrace();
+		}
+
+		return partida;
 	}
 
 }
